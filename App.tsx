7c991fbbbfd2103649a069b90ef7f5345e51b3d6
Lawliet1200 <lopes.lucas2509@gmail.com>
2025-11-17 22:24:46 -0300

    Adicionando pastas e arquivos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, Button } from 'react-native';
+import styles from './styles';
+
+const imagesLogos = {
+  donjuan: require('./assets/icon_donjuan.png'),
+} as const;
+
+type LogoName = keyof typeof imagesLogos;
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.textHeader}>Procurar</Text>
+      <Text style={styles.textHeader}>Preços</Text>
+      <Image source={imagesLogos.donjuan} style={styles.imageHeader}/>
+      <Text style={styles.textHeader}>Contato</Text>
+      <Text style={styles.textHeader}>Sobre</Text>
+    </View>
+  )
+}
+
+const ContentClient = (
+  {name='Melhores ...', children='...'}:
+  {name: string, children?: React.ReactNode}
+) => {
+  return (
+    <View style={styles.contentSessions}>
+      <View style={styles.contentRatings}>
+        <Text style={styles.contentText}>{name}</Text>
+        {children}
+      </View>
+    </View>
+  );
+};
+
+const ClientZone = ({
+  name='Barbearia', 
+  ratings=0.0,
+  price=0.0, 
+  image='donjuan'
+}: {
+  name: string,
+  ratings: number,
+  price: number,
+  image: LogoName,
+}
+
+) => {
+  return (
+
+        <View style={styles.contentImages}>
+          <View style={styles.contentImagesProducts}>
+            <Image source={imagesLogos[image]} style={styles.contentImagesImage}/>
+            <View style={styles.contentImagesText}>
+              <Text style={styles.contentImagesProductsTextTitle}>{name}</Text>
+              <Text style={styles.contentImagesProductsText}>{ratings}</Text>
+              <Text style={styles.contentImagesProductsText}>{price}</Text>
+            </View>
+          </View>
+        </View>
+  );
+};
+
+const starRatingsCreator = (ratings: number) => {
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header></Header>
+      <ContentClient name='Melhores avaliados'>
+        <ClientZone name='Barbearia do Zé' ratings={4.5} price={3.5} image={'donjuan'} />
+      </ContentClient>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ styles.js
@@ -0,0 +1,87 @@
+import { StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  body: {
+    fontFamily: 'Arial',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#101010',
+    alignItems: 'center',
+    alignContent: 'center'
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  textHeader: {
+    fontSize: 12,
+    color: '#d4d4d8',
+  },
+  imageHeader: {
+    width: 40,
+    height: 40,
+  },
+  contentSessions: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '90%',
+    marginTop: 300,
+  },
+  contentRatings: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    width: '100%',
+    height: 200,
+  },
+  contentText: {
+    fontSize: 20,
+    fontWeight: '100',
+    color: '#d4d4d8',
+  },
+  contentImages: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    paddingTop: 20
+  },
+  contentImagesProducts: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: 270,
+    height: 145,
+    backgroundColor: '#1e3a8a',
+    borderRadius: 10,
+    paddingTop: 7
+  },
+    contentImagesImage: {
+    width: 130,
+    height: 130,
+  },
+  contentImagesText: {
+  },
+  contentImagesProductsTextTitle: {
+    fontSize: 18,
+    color: '#d4d4d8',
+    marginTop: 5,
+    fontWeight: '400',
+    marginBottom: 20
+  },
+  contentImagesProductsText: {
+    fontSize: 16,
+    color: '#d4d4d8',
+    fontWeight: '200',
+
+  }
+});
+
+export default styles;
